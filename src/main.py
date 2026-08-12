e4d4277a7fe6ba4ea7b7f0643470e1bbff484f52
unknown <tamalikabiswas216@gmail.com>
2026-08-12 15:11:40 +1000

    using regex

--- src/main.py
+++ src/main.py
@@ -1,28 +1,27 @@
+import re
 with open("data/raw/nasa_aug95.log", "r") as file:
-    # line_count = 0
-#     for line in file:
-#         line_count += 1
-#         print(line)
-# print(f"total line: {line_count}")
-    # first_line = file.readline()
-    # print(first_line)
-    # print(first_line.split())
-    # parts = first_line.split()
-    # print(parts)
-    # print(len(parts))
-    # print(parts[0])
-    # print(parts[8])
-    # print(parts[9])
-    count = 0
-    for line in file:
-        parts = line.split()
-        # print([parts])
-        if len(parts)>= 10:
-            client = parts[0]
-            status = parts[8]
-            bytes_sent = parts[9]
-            print(client, status, bytes_sent)
-        count +=1
-        if count == 5:
-            break
+    # status_counts = {}
+    # count = 0
+    # for line in file:
+    #     parts = line.split()
+    #     if len(parts)>= 10:
+    #         client = parts[0]
+    #         status = parts[8]
+    #         bytes_sent = parts[9]
+    #     #     print(client, status, bytes_sent)
+    #         status = status.strip(".,!?:;\"'")
+
+    #         if status.isdigit():
+    #             status_counts[status] = status_counts.get(status,0) + 1
+    #         else:
+    #             if count < 6:
+    #                 print(line)
+    #                 count +=1
+    # print(status_counts)
+    first_line = file.readline()
+Timestamp = re.search(r"\[(.*?)\]", first_line)
+Request = re.search(r'"(.*?)"', first_line)
+print(Timestamp.group(1))
+print(Request.group(1))
+      
 
