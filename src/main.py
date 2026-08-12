76506562aa8b52d3a5003bda33dd46d5fea92516
unknown <tamalikabiswas216@gmail.com>
2026-08-12 20:08:10 +1000

    _

--- src/main.py
+++ src/main.py
@@ -1,27 +1,21 @@
 import re
 with open("data/raw/nasa_aug95.log", "r") as file:
-    # status_counts = {}
-    # count = 0
-    # for line in file:
-    #     parts = line.split()
-    #     if len(parts)>= 10:
-    #         client = parts[0]
-    #         status = parts[8]
-    #         bytes_sent = parts[9]
-    #     #     print(client, status, bytes_sent)
-    #         status = status.strip(".,!?:;\"'")
-
-    #         if status.isdigit():
-    #             status_counts[status] = status_counts.get(status,0) + 1
-    #         else:
-    #             if count < 6:
-    #                 print(line)
-    #                 count +=1
-    # print(status_counts)
+    # for line in file:   
     first_line = file.readline()
-Timestamp = re.search(r"\[(.*?)\]", first_line)
-Request = re.search(r'"(.*?)"', first_line)
-print(Timestamp.group(1))
-print(Request.group(1))
-      
-
+    def parse_log_line(line):
+        parts = line.split()
+        client = parts[0]
+        match = re.search(
+            r'^(.*?) - - \[(.*?)\] "(.*?)" (\d{3}) (\d+|-)', 
+            first_line)
+        if not match:
+            return None
+        return {
+            "client" : match.group(1),
+            "timestamp": match.group(2),
+            "request": match.group(3),
+            "status" : match.group(4),
+            "bytes_sent": match.group(5)
+        }
+result = parse_log_line(first_line)
+print(result)
