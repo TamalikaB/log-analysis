0240df77bd5e19e7fec7f70467bd26c400b6b2a0
unknown <tamalikabiswas216@gmail.com>
2026-08-15 21:24:57 +1000

    reading the data using regex

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 import re
+count =0
+def parse_log_line(line):
+    match = re.search(
+        r'^(.*?) - - \[(.*?)\] "(.*?)" (\d{3}) (\d+|-)', 
+        line)
+    if not match:
+        return None
+    return {
+        "client" : match.group(1),
+        "timestamp": match.group(2),
+        "request": match.group(3),
+        "status" : match.group(4),
+        "bytes_sent": match.group(5)
+    }
+
 with open("data/raw/nasa_aug95.log", "r") as file:
-    # for line in file:   
-    first_line = file.readline()
-    def parse_log_line(line):
-        parts = line.split()
-        client = parts[0]
-        match = re.search(
-            r'^(.*?) - - \[(.*?)\] "(.*?)" (\d{3}) (\d+|-)', 
-            first_line)
-        if not match:
-            return None
-        return {
-            "client" : match.group(1),
-            "timestamp": match.group(2),
-            "request": match.group(3),
-            "status" : match.group(4),
-            "bytes_sent": match.group(5)
-        }
-result = parse_log_line(first_line)
-print(result)
+    for line in file:   
+        result = parse_log_line(line)
+        count += 1
+        if count<= 5:
+            print(result)
+        else:
+            break
